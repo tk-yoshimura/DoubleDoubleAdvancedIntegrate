b9349a5ba6cb7c194a336f9b3ce17cbb39ceb438
tk-yoshimura <yoshimura8192@gmail.com>
2024-08-05 02:42:07 +0900

    add: surface integral

---
+++ DoubleDoubleAdvancedIntegrate/SurfaceIntegral.cs
@@ -0,0 +1,70 @@
+﻿using DoubleDouble;
+using DoubleDoubleIntegrate;
+using System.Collections.ObjectModel;
+
+namespace DoubleDoubleAdvancedIntegrate {
+    public static class SurfaceIntegral {
+        public class Surface(ddouble x, ddouble y, ddouble z) {
+            public ddouble x = x, y = y, z = z;
+        }
+
+        public class Partials((ddouble dxdu, ddouble dydu, ddouble dzdu) du, (ddouble dxdv, ddouble dydv, ddouble dzdv) dv) {
+            public ddouble dxdu = du.dxdu, dydu = du.dydu, dzdu = du.dzdu, dxdv = dv.dxdv, dydv = dv.dydv, dzdv = dv.dzdv;
+        }
+
+        public static (ddouble value, ddouble error) Integrate(
+            Func<ddouble, ddouble, ddouble, ddouble> f,
+            Func<ddouble, ddouble, Surface> s,
+            Func<ddouble, ddouble, Partials> d,
+            (ddouble min, ddouble max) u_range, (ddouble min, ddouble max) v_range, 
+            GaussKronrodOrder order = GaussKronrodOrder.G31K63) {
+
+            ReadOnlyCollection<(ddouble x, ddouble wg, ddouble wk)> ps = GaussKronrodPoints.Table[order];
+
+            ddouble sg = ddouble.Zero, sk = ddouble.Zero;
+            ddouble ru = u_range.max - u_range.min, rv = v_range.max - v_range.min;
+
+            if (!ddouble.IsFinite(ru)) {
+                throw new ArgumentException("Invalid integation interval.", nameof(u_range));
+            }
+            if (!ddouble.IsFinite(rv)) {
+                throw new ArgumentException("Invalid integation interval.", nameof(v_range));
+            }
+
+            for (int i = 0; i < ps.Count; i++) {
+                ddouble u = ps[i].x * ru + u_range.min;
+                
+                for (int j = 0; j < ps.Count; j++) {
+                    ddouble v = ps[j].x * rv + v_range.min;
+
+                    Surface surface = s(u, v);
+                    Partials partial = d(u, v);
+                    ddouble value = f(surface.x, surface.y, surface.z);
+
+                    ddouble dsduv = ddouble.Hypot(
+                        partial.dydu * partial.dzdv - partial.dzdu * partial.dydv, 
+                        partial.dzdu * partial.dxdv - partial.dxdu * partial.dzdv, 
+                        partial.dxdu * partial.dydv - partial.dydu * partial.dxdv
+                    );
+
+                    ddouble g = value * dsduv;
+
+                    sk += ps[i].wk * ps[j].wk * g;
+
+                    if ((i & 1) == 1 && (j & 1) == 1) {
+                        sg += ps[i].wg * ps[j].wg * g;
+                    }
+                }
+            }
+
+            ddouble uv = ru * rv;
+
+            sk *= uv;
+            sg *= uv;
+
+            ddouble error = ddouble.Abs(sk - sg);
+
+            return (sk, error);
+        }
+    }
+}
---
+++ DoubleDoubleAdvancedIntegrateTest/SurfaceIntegralTests.cs
@@ -0,0 +1,121 @@
+﻿using DoubleDoubleAdvancedIntegrate;
+using DoubleDouble;
+
+namespace DoubleDoubleAdvancedIntegrateTest {
+    [TestClass()]
+    public class SurfaceIntegralTests {
+        [TestMethod()]
+        public void Test1() {
+            (ddouble value, ddouble error) = SurfaceIntegral.Integrate(
+                (x, y, z) => x + y + z,
+                (u, v) => new SurfaceIntegral.Surface(u, v, u * u + v * v),
+                (u, v) => new SurfaceIntegral.Partials((1, 0, 2 * u), (0, 1, 2 * v)),
+                (0, 1), (0, 1)
+            );
+
+            Console.WriteLine(value);
+            Console.WriteLine($"{error:e4}");
+
+            Assert.IsTrue(ddouble.Abs(value - 3.4684) < 1e-3);
+        }
+
+        [TestMethod()]
+        public void Test2() {
+            (ddouble value, ddouble error) = SurfaceIntegral.Integrate(
+                (x, y, z) => x * x * y * y * y,
+                (u, v) => new SurfaceIntegral.Surface(u, v, 0),
+                (u, v) => new SurfaceIntegral.Partials((1, 0, 0), (0, 1, 0)),
+                (0, 1), (0, 1)
+            );
+
+            Console.WriteLine(value);
+            Console.WriteLine($"{error:e4}");
+
+            Assert.IsTrue(ddouble.Abs(value - 1d / 12) < 1e-14);
+        }
+
+        [TestMethod()]
+        public void Test3() {
+            (ddouble value, ddouble error) = SurfaceIntegral.Integrate(
+                (x, y, z) => ddouble.Square(x - y),
+                (u, v) => new SurfaceIntegral.Surface(u, v, 0),
+                (u, v) => new SurfaceIntegral.Partials((1, 0, 0), (0, 1, 0)),
+                (0, 2), (0, 1)
+            );
+
+            Console.WriteLine(value);
+            Console.WriteLine($"{error:e4}");
+
+            Assert.IsTrue(ddouble.Abs(value - 4d / 3) < 1e-14);
+        }
+
+        [TestMethod()]
+        public void Test4() {
+            (ddouble value, ddouble error) = SurfaceIntegral.Integrate(
+                (x, y, z) => 2 * x + y,
+                (u, v) => new SurfaceIntegral.Surface(u, v * (1 - u), 0),
+                (u, v) => new SurfaceIntegral.Partials((1, -v, 0), (0, 1 - u, 0)),
+                (0, 1), (0, 1)
+            );
+
+            Console.WriteLine(value);
+            Console.WriteLine($"{error:e4}");
+
+            Assert.IsTrue(ddouble.Abs(value - 0.5) < 1e-14);
+        }
+
+        [TestMethod()]
+        public void Test5() {
+            (ddouble value, ddouble error) = SurfaceIntegral.Integrate(
+                (x, y, z) => 1,
+                (theta, r) => new SurfaceIntegral.Surface(ddouble.Cos(theta) * r, ddouble.Sin(theta) * r, 0),
+                (theta, r) => new SurfaceIntegral.Partials(
+                    (-ddouble.Sin(theta) * r, ddouble.Cos(theta) * r, 0), 
+                    (ddouble.Cos(theta), ddouble.Sin(theta), 0)
+                ),
+                (0, ddouble.PI * 2), (0, 1)
+            );
+
+            Console.WriteLine(value);
+            Console.WriteLine($"{error:e4}");
+
+            Assert.IsTrue(ddouble.Abs(value - ddouble.PI) < 1e-14);
+        }
+
+        [TestMethod()]
+        public void Test6() {
+            (ddouble value, ddouble error) = SurfaceIntegral.Integrate(
+                (x, y, z) => y,
+                (theta, r) => new SurfaceIntegral.Surface(ddouble.Cos(theta) * r, ddouble.Sin(theta) * r, 0),
+                (theta, r) => new SurfaceIntegral.Partials(
+                    (-ddouble.Sin(theta) * r, ddouble.Cos(theta) * r, 0), 
+                    (ddouble.Cos(theta), ddouble.Sin(theta), 0)
+                ),
+                (0, ddouble.PI / 2), (0, 1)
+            );
+
+            Console.WriteLine(value);
+            Console.WriteLine($"{error:e4}");
+
+            Assert.IsTrue(ddouble.Abs(value - 1d / 3) < 1e-14);
+        }
+
+        [TestMethod()]
+        public void Test7() {
+            (ddouble value, ddouble error) = SurfaceIntegral.Integrate(
+                (x, y, z) => x * x + y * y,
+                (theta, r) => new SurfaceIntegral.Surface(ddouble.Cos(theta) * r, ddouble.Sin(theta) * r, 0),
+                (theta, r) => new SurfaceIntegral.Partials(
+                    (-ddouble.Sin(theta) * r, ddouble.Cos(theta) * r, 0), 
+                    (ddouble.Cos(theta), ddouble.Sin(theta), 0)
+                ),
+                (0, ddouble.PI * 2), (1, 2)
+            );
+
+            Console.WriteLine(value);
+            Console.WriteLine($"{error:e4}");
+
+            Assert.IsTrue(ddouble.Abs(value - 15 * ddouble.PI / 2) < 1e-14);
+        }
+    }
+}
