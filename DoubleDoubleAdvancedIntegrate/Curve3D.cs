f7d6d44724e2dc2c481f37af4cfa3bd378860856
tk-yoshimura <yoshimura8192@gmail.com>
2024-08-07 03:18:20 +0900

    add: curve.circle3d

--- DoubleDoubleAdvancedIntegrate/Curve3D.cs
+++ DoubleDoubleAdvancedIntegrate/Curve3D.cs
@@ -1,4 +1,5 @@
 ﻿using DoubleDouble;
+using DoubleDoubleAdvancedIntegrate.Utils;
 
 namespace DoubleDoubleAdvancedIntegrate {
     public class Curve3D {
@@ -24,6 +25,74 @@ namespace DoubleDoubleAdvancedIntegrate {
             return new(
                 t => (v0.x + t * dx, v0.y + t * dy, v0.z + t * dz),
                 t => (dx, dy, dz)
+            );
+        }
+
+        public static Curve3D Circle((ddouble x, ddouble y, ddouble z) normal) {
+            ((ddouble x, ddouble y, ddouble z) a, (ddouble x, ddouble y, ddouble z) b) = VectorUtil.OrthoVector(normal);
+
+            return new(
+                t => { 
+                    ddouble c = ddouble.Cos(t), s = ddouble.Sin(t);
+
+                    return (a.x * c + b.x * s, a.y * c + b.y * s, a.z * c + b.z * s);
+                },
+                t => { 
+                    ddouble c = ddouble.Cos(t), s = ddouble.Sin(t);
+
+                    return (-a.x * s + b.x * c, -a.y * s + b.y * c, -a.z * s + b.z * c);
+                }
+            );
+        }
+
+        public static Curve3D Circle(ddouble radius, (ddouble x, ddouble y, ddouble z) normal) {
+            ((ddouble x, ddouble y, ddouble z) a, (ddouble x, ddouble y, ddouble z) b) = VectorUtil.OrthoVector(normal);
+
+            return new(
+                t => { 
+                    ddouble c = radius * ddouble.Cos(t), s = radius * ddouble.Sin(t);
+
+                    return (a.x * c + b.x * s, a.y * c + b.y * s, a.z * c + b.z * s);
+                },
+                t => { 
+                    ddouble c = radius * ddouble.Cos(t), s = radius * ddouble.Sin(t);
+
+                    return (-a.x * s + b.x * c, -a.y * s + b.y * c, -a.z * s + b.z * c);
+                }
+            );
+        }
+
+        public static Curve3D Circle((ddouble x, ddouble y, ddouble z) center, (ddouble x, ddouble y, ddouble z) normal) {
+            ((ddouble x, ddouble y, ddouble z) a, (ddouble x, ddouble y, ddouble z) b) = VectorUtil.OrthoVector(normal);
+
+            return new(
+                t => { 
+                    ddouble c = ddouble.Cos(t), s = ddouble.Sin(t);
+
+                    return (center.x + a.x * c + b.x * s, center.y + a.y * c + b.y * s, center.z + a.z * c + b.z * s);
+                },
+                t => { 
+                    ddouble c = ddouble.Cos(t), s = ddouble.Sin(t);
+
+                    return (-a.x * s + b.x * c, -a.y * s + b.y * c, -a.z * s + b.z * c);
+                }
+            );
+        }
+
+        public static Curve3D Circle((ddouble x, ddouble y, ddouble z) center, ddouble radius, (ddouble x, ddouble y, ddouble z) normal) {
+            ((ddouble x, ddouble y, ddouble z) a, (ddouble x, ddouble y, ddouble z) b) = VectorUtil.OrthoVector(normal);
+
+            return new(
+                t => { 
+                    ddouble c = radius * ddouble.Cos(t), s = radius * ddouble.Sin(t);
+
+                    return (center.x + a.x * c + b.x * s, center.y + a.y * c + b.y * s, center.z + a.z * c + b.z * s);
+                },
+                t => { 
+                    ddouble c = radius * ddouble.Cos(t), s = radius * ddouble.Sin(t);
+
+                    return (-a.x * s + b.x * c, -a.y * s + b.y * c, -a.z * s + b.z * c);
+                }
             );
         }
     }
